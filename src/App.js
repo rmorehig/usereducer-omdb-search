c9eb5739c38a0b502cca8dbcdc80301b5da8d2c7
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-10-30 15:30:43 +0100

    implement useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
@@ -6,43 +6,80 @@ import Search from "./components/Search";
 
 const MOVIE_API_URL = `${process.env.REACT_APP_MOVIE_API_URL}star+wars`;
 
+const initialState = {
+  loading: true,
+  movies: [],
+  error: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const getMovies = async () => {
       const response = await fetch(MOVIE_API_URL);
       const data = await response.json();
-      setMovies(data.Search);
-      setIsLoading(false);
+      dispatch({
+        type: "SEARCH_MOVIES_SUCCESS",
+        payload: data.Search
+      });
     };
     getMovies();
   }, []);
 
   const search = async searchValue => {
-    setIsLoading(true);
-    setErrorMessage(null);
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
     const response = await fetch(
       `${process.env.REACT_APP_MOVIE_API_URL}${searchValue}`
     );
     const data = await response.json();
     data.Response === "True"
-      ? setMovies(data.Search)
-      : setErrorMessage(data.Error);
-    setIsLoading(false);
+      ? dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: data.Search
+        })
+      : dispatch({
+          type: "SEARCH_MOVIES_ERROR",
+          payload: data.Error
+        });
   };
-
+  const { movies, error, loading } = state;
   return (
     <div className="App">
       <Header text="OMDB SEARCH" />
       <Search search={search} />
       <div className="movies">
-        {isLoading && !errorMessage ? (
+        {loading && !error ? (
           <span>Loading...</span>
-        ) : errorMessage ? (
-          <div className="errorMessage">{errorMessage}</div>
+        ) : error ? (
+          <div className="error">{error}</div>
         ) : (
           movies.map((movie, index) => (
             <Movie key={`${index}-${movie.Title}`} movie={movie} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = props => {
   return (
     <header className="App-header">
-      <h2>{props.text}</h2>
+      <a href="/">
+        <h2>{props.text}</h2>
+      </a>
     </header>
   );
 };
