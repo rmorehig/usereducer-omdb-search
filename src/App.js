efac4eb7184978c5e50b736cd3d0bcd384df0d39
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-10-30 15:01:29 +0100

    refactoring data fetchng

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from "react";
-import "../App.css";
+import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
 import Search from "./components/Search";
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; // you should replace this with yours
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=star+wars&apikey=4a3b711b";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    fetch(MOVIE_API_URL)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
-      });
+    const getMovies = async () => {
+      const response = await fetch(MOVIE_API_URL);
+      const data = await response.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    };
+    getMovies();
   }, []);
 
-  const search = searchValue => {
-    setLoading(true);
+  const search = async searchValue => {
+    setIsLoading(true);
     setErrorMessage(null);
 
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.Response === "True") {
-          setMovies(jsonResponse.Search);
-          setLoading(false);
-        } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
-        }
-      });
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`
+    );
+    const data = await response.json();
+    if (data.Response === "True") {
+      setMovies(data.Search);
+      setIsLoading(false);
+    } else {
+      setErrorMessage(data.Error);
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="OMDB SEARCH" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
-        {loading && !errorMessage ? (
-          <span>loading...</span>
+        {isLoading && !errorMessage ? (
+          <span>Loading...</span>
         ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
